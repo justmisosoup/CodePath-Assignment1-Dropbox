f9c56e5b48c8d001242ac12d35ba48dde4596fd1
Sara Menefee <sara@menef.ee>
2014-09-15 19:02:35 -0700

    Dropbox Log-in Flow
    Week 1 Assignment

---
+++ Dropbox Log-in Flow/PhotosExistingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotosExistingViewController.swift
+//  Dropbox Log-in Flow
+//
+//  Created by Sara Menefee on 9/14/14.
+//  Copyright (c) 2014 Sara Menefee. All rights reserved.
+//
+
+import UIKit
+
+class PhotosExistingViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad(scrollView.contentSize = CGSize(width: 320, height: 1000))
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dropbox Log-in Flow/SignIn3ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignIn3ViewController.swift
+//  Dropbox Log-in Flow
+//
+//  Created by Sara Menefee on 9/14/14.
+//  Copyright (c) 2014 Sara Menefee. All rights reserved.
+//
+
+import UIKit
+
+class SignIn3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func clickBackView(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
